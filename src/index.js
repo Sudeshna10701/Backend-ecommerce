9e6d91d14a28c6728dcd2b14def3e49df017f8a4
Sudeshna10701 <sudeshna10dutta@gmail.com>
2021-12-13 23:19:55 +0530

    workflow added

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
 app.get('/', (req, res) => {
-    res.send(`Server running at ${port} (Deployed from workflow)`)
+    res.send(`Server running at ${port}`)
 })
 app.listen(port, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
