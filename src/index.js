bf50ee9e9f522f2612c34a403381fc5c095a9bef
Sudeshna10701 <sudeshna10dutta@gmail.com>
2021-12-13 21:35:33 +0530

    change config

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ app.use("/api/v1/auth", authroute)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
+console.log('hi')
 app.get('/', (req, res) => {
     res.send(`Server running at ${port}  `)
 })
-
 app.listen(port, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
 })
